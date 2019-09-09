57cd33d496ffc13f71c6031fffcb02da01d34d3b
Elijah Guerrero <elijahguerrero97@gmail.com>
2019-09-09 10:19:51 -0500

    added new branch

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function btnDropClick(){
     console.log("btnDrop executed!")
     $('.content-drop').toggleClass("hide");
 }
+
+$('.email').click(function(e) {  
+    document.getElementById('email').style.backgroundColor="rgba(0, 0, 0, 0.009)";
+    document.getElementById('email').style.borderColor="grey";
+
+});
